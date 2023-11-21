2d6f5de73044304972c4b049866314d7cb02f837
Karol ZIELINSKI <karol.zielinski@septeo.com>
2023-11-21 17:19:48 +0100

    Update templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,16 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TrickRepository;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(TrickRepository $repo)
     {
+        // Get 10 tricks from position 0
+        $tricks = $repo->findBy([], ['created_at' => 'DESC'], 10, 0);
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'tricks' => $tricks
         ]);
     }
 }
